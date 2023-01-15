15c4d635501a7073ae83ca30a45e0d27de6b240a
kalivm90 <kalivm90@gmail.com>
2023-01-15 15:30:14 -0800

    added compatibility for babel 25%>

--- src/index.js
+++ src/index.js
@@ -5,3 +5,11 @@ const image = new Image();
 image.src = Icon
 
 document.querySelector(".image").appendChild(image)
+
+// test babel
+const math = () => {
+    let l = "test"
+    return [1,2,3,4,5].reduce((acc, cur) => acc += cur)
+}
+
+console.log(math());
--- webpack.config.js
+++ webpack.config.js
@@ -35,80 +35,16 @@ module.exports = {
       {
         test: /\.(svg|png|jpeg|jpg)$/,
         type: 'asset/resource'
+
       },
-    ],
-  },
-}
-
-/* OG */
-/* 
-
-const path = require('path');
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-
-module.exports = {
-  mode: 'development',
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
-  devServer: {
-    static: {
-      directory: path.resolve(__dirname, 'dist'),
-    },
-    port: 3000,
-    open: true,
-    hot: true,
-    compress: true,
-    historyApiFallback: true,
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: 'Tailwinds Template',
-      template: "./src/template.html"
-    }),
-  ],
-  module: {
-    rules: [
+      // BABEL
       {
-        test: /\.css$/i,
-        include: path.resolve(__dirname, 'src'),
-        use: ['style-loader', 'css-loader', 'postcss-loader'],
-      },
-
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: {
+            loader: 'babel-loader',
+        }
+      }
     ],
   },
 }
-
-*/
-
-
-
-
-/* 
-
-          {
-            test: /\.(gif|png|jpe?g)$/,
-            use: [
-              {
-                loader: 'file-loader',
-                options: {
-                  name: '[name].[ext]',
-                  outputPath: 'assets/images/'
-                }
-              }
-            ]
-          },
-          {
-            test:/\.html$/,
-            use: [
-              'html-loader'
-            ]
-          },
-          {
-            test: /\.(svg)$/,
-            type: 'asset/resource'
-          },
-
-*/
